5dbaed7f6d444887ce9fadf92c5653b93a3f8ab9
JulesEfrei <jules.bruzeau@efrei.net>
2023-02-28 11:04:19 +0000

    Add : cache & cache invalidation

--- routes/placeRoute.js
+++ routes/placeRoute.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const placeController = require("../controllers/placeController");
 const { verifyToken } = require("../utils/verifyToken");
+const cache = require("../utils/cache");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => placeController.getAll(res));
+router.get(
+  "/",
+  cache.get,
+  async (req, res) => placeController.getAll(res),
+  cache.set
+);
 
-router.post("/", verifyToken, async (req, res) =>
+router.post("/", verifyToken, cache.clear, verifyToken, async (req, res) =>
   placeController.addOne(req, res)
 );
 
 router
   .route("/:id")
-  .get(async (req, res) => placeController.getOne(req, res))
-  .delete(verifyToken, async (req, res) => placeController.deleteOne(req, res))
-  .patch(verifyToken, async (req, res) => placeController.updateOne(req, res));
+  .get(
+    cache.get,
+    async (req, res) => placeController.getOne(req, res),
+    cache.set
+  )
+  .delete(verifyToken, cache.clear, async (req, res) =>
+    placeController.deleteOne(req, res)
+  )
+  .patch(verifyToken, cache.clear, async (req, res) =>
+    placeController.updateOne(req, res)
+  );
 
 module.exports = router;
